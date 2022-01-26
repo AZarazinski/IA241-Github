e4a4483a51123f434d76cb1306f7defd493dfcd9
EC2 Default User <ec2-user@ip-172-31-64-123.ec2.internal>
2022-01-26 21:38:03 +0000

    lec2

---
+++ LEC2.py
@@ -0,0 +1,56 @@
+"""
+this is a regional comment
+
+Lec2
+
+"""
+
+# ('hello world') # this is a ling comment
+
+#print( type( 123 ) )
+
+#print( type (123.) ) 
+
+#print( type( '123' ) ) 
+
+#print( "hello world"  ) 
+
+#print ('hello world')
+
+#print( "Tom's cat" ) 
+
+#print('Hello World')
+
+#print('Hello World'.upper() )
+
+#print('Hello World'.lower() )
+
+#print('  Hello World  ' )
+#print('  Hello World  '.strip())
+
+#print('Hello World'.split())
+#print('Hello, World'.split(','))
+
+#print('Hello, World'.split('o'))
+
+# (2. ** 3)
+
+my_int = 123
+
+print(my_int)
+
+my_int = 321
+
+print(my_int)
+
+my_float = 123.
+
+my_str = 'hello world'
+
+print(my_float + 1 )
+
+print(my_str.upper())
+
+_my_int = 123
+
+print(_my_int)
